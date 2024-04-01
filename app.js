ab7a78e21549fd01250f19ac3fac3334547d4033
Harun Sahinol <mehmethsh@gmail.com>
2024-04-01 16:43:21 +0300

    dotenv added

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ const tourRouter = require("./routes/tourRouter");
 const app = express();
 
 app.use(express.json());
-app.use(morgan("dev"));
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log("Hello from the middleware");
--- controllers/tourController.js
+++ controllers/tourController.js
@@ -6,11 +6,22 @@ const tours = JSON.parse(
 
 exports.checkID = (req, res, next, val) => {
   console.log(`Tour id is ${val}`);
-  const id = req.params.id * 1; // req.params.id is a string, so we need to convert it to a number
+  // req.params.id is a string, so we need to convert it to a number
+  const id = req.params.id * 1;
   if (req.params.id * 1 > tours.length) {
     return res.status(404).json({
       status: "fail",
       message: "Invalid ID",
+    });
+  }
+  next();
+};
+
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: "fail",
+      message: "Missing name or price",
     });
   }
   next();
@@ -29,8 +40,7 @@ exports.getAllTours = (req, res) => {
 };
 
 exports.getTour = (req, res) => {
-  // :id is a url parameter and '?' makes it optional
-
+  const id = req.params.id * 1;
   const tour = tours.find((el) => el.id === id); // find() returns the first element that satisfies the condition
 
   res.status(200).json({
@@ -73,13 +83,6 @@ exports.updateTour = (req, res) => {
 };
 
 exports.deleteTour = (req, res) => {
-  if (req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: "fail",
-      message: "Invalid ID",
-    });
-  }
-
   res.status(204).json({
     status: "success",
     data: null,
